4b245d8c486c2a37a0d0449ee7043b04b233ca98
Mardav Chirag Gandhi <gandhi.mardav@gmail.com>
2023-12-08 23:47:23 +0530

    server: add tableland

--- server/app/schema/iot_data.py
+++ server/app/schema/iot_data.py
@@ -1,8 +1,8 @@
 from pydantic import BaseModel
 
 class CropData(BaseModel):
-    temperature: float
-    pressure: float
+    temperature: str
+    pressure: str
     moisture: int
     # heart_rate_curr: int
     # heart_rate_avg: int
